fc9c1db471661ba6ab59d261008bad91fa84fea6
Jim Macur <jim.macur@gmail.com>
2025-01-05 08:59:35 -0700

    makes homepage and buttons responsive

--- src/views/HomeScreen.vue
+++ src/views/HomeScreen.vue
@@ -1,21 +1,21 @@
 <template>
-  <div>
+  <div class="min-h-screen flex flex-col justify-center items-center space-y-4 sm:space-y-6 md:space-y-8">
     <!-- Title -->
-    <h1 class="text-8xl font-bold mb-8">Chess War</h1>
+    <h1 class="text-6xl sm:text-7xl md:text-8xl font-bold text-center">Chess War</h1>
 
     <!-- Logo -->
     <div>
       <img
         src="../../public/assets/logo.jpg"
         alt="Chess War Logo"
-        class="max-w-lg rounded-lg shadow-lg mb-8"
+        class="max-w-xs sm:max-w-md md:max-w-lg lg:max-w-lg rounded-lg shadow-2xl mx-auto "
       />
     </div>
 
     <!-- Buttons -->
-    <div>
-      <button @click="openModal('playVsComputer')" class="btn">Play vs. Computer</button>
-      <router-link to="/game/friend" class="btn inline-block text-center">Play vs. Friend</router-link>
+    <div class="w-full max-w-xs sm:max-w-md md:max-w-lg flex flex-col sm:flex-row items-center md: justify-between">
+      <button @click="openModal('playVsComputer')" class="btn">Play vs. Bot</button>
+      <router-link to="/game/friend" class="btn inline-block">Play vs. Friend</router-link>
       <button @click="openModal('about')" class="btn">About</button>
     </div>
 
