423337870b0251559a2a96e01b401daf2264ccc2
Jim Macur <jim.macur@gmail.com>
2025-01-11 11:00:58 -0700

    fix the padding issue in the scorecard at size large

--- src/components/features/ScoreCard.vue
+++ src/components/features/ScoreCard.vue
@@ -5,7 +5,7 @@
   ]">
     <!-- Avatar -->
     <div
-      class="hidden sm:flex items-center justify-center w-12 sm:w-16 md:w-18 lg-sm:w-20  lg:w-28 h-12 sm:h-16 md:h-18 lg-sm:h-20 lg:h-28 rounded-full bg-gray-700"
+      class="hidden sm:flex items-center justify-center w-12 sm:w-16 md:w-18 lg-sm:w-20 lg:w-28 h-12 sm:h-16 md:h-18 lg-sm:h-20 lg:h-28 rounded-full bg-gray-700"
     >
       <img
         :src="avatarSrc || '/src/assets/default-avatar.png'"
@@ -15,7 +15,7 @@
     </div>
     <!-- Score Box -->
     <div
-      class="bg-gray-800 text-white p-2 rounded shadow-lg flex flex-col items-center justify-center w-20 sm:w-24 lg-sm:w-28  h-14 sm:h-18 lg-sm:h-20"
+      class="bg-gray-800 text-white p-10 rounded shadow-lg flex flex-col items-center justify-center w-20 sm:w-24 lg-sm:w-28  h-14 sm:h-18 lg-sm:h-20"
     >
       <h2
         class="text-sm sm:text-base lg-sm:text-lg md:text-lg font-semibold text-center"
